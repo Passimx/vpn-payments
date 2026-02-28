ab7e260087d7a99582820501087e045a3e3bbf40
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-02-28 18:27:16 +0800

    fix: add cache for price

--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -3,7 +3,12 @@ import { CryptoPriceType } from './types/crypto-price.type';
 
 @Injectable()
 export class TransactionsService {
+  private cache: CryptoPriceType | null = null;
+  private readonly TTL = 10 * 60 * 1000;
+
   public async getCurrencyPrice() {
+    if (this.cache) return this.cache;
+
     const date = new Date();
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы 0-11
@@ -15,6 +20,11 @@ export class TransactionsService {
     ).catch(() => {});
     if (!response) return;
 
-    return (await response.json()) as CryptoPriceType;
+    this.cache = (await response.json()) as CryptoPriceType;
+    setTimeout(() => {
+      this.cache = null;
+    }, this.TTL);
+
+    return this.cache;
   }
 }
