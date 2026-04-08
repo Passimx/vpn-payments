767e9ea6d8bd4b0ec69a3fe5bda0cebe53a74bbf
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-04-08 17:42:51 +0800

    refactor: add logger time

--- src/common/logger/logger.ts
+++ src/common/logger/logger.ts
@@ -2,20 +2,22 @@ const endColor = '\x1B[0m'; // ansi color escape codes
 const infoColor = '\x1B[34m';
 const errorColor = '\x1B[31m';
 const debugColor = '\x1B[33m';
+const timeColor = '\x1B[90m';
 
 const log = (level: string, msg?: unknown, ...optionalParams: unknown[]) => {
-  let prefix = `${infoColor}info:${endColor}`;
+  let prefix = `${infoColor}Info${endColor}`;
+  const time = getTimeString();
 
   switch (level) {
     case 'error':
-      prefix = `${errorColor}error:${endColor}`;
+      prefix = `${errorColor}Error${endColor}`;
       break;
     case 'debug':
-      prefix = `${debugColor}debug:${endColor}`;
+      prefix = `${debugColor}Debug${endColor}`;
       break;
   }
 
-  console.log(prefix, msg, ...optionalParams);
+  console.log(`[${prefix}]`, time, msg, ...optionalParams);
 };
 
 export const logger = {
@@ -25,4 +27,24 @@ export const logger = {
     log('error', msg, ...optionalParams),
   debug: (msg?: unknown, ...optionalParams: unknown[]) =>
     log('debug', msg, ...optionalParams),
+};
+
+export const getTimeString = () => {
+  const now = new Date();
+  const timeStr = now.toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  });
+
+  const offsetMinutes = -now.getTimezoneOffset();
+  const offsetHours = Math.floor(Math.abs(offsetMinutes) / 60);
+  const sign = offsetMinutes >= 0 ? '+' : '-';
+  const offsetStr = `UTC${sign}${offsetHours}`;
+
+  return `${timeColor}[${timeStr} ${offsetStr}]${endColor}`;
 };
