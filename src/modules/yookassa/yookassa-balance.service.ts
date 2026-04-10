d49486a0aba9fa5f57c6b3495e9f99dae2ad26a6
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-04-10 18:28:12 +0800

    fix: correct redirect url

--- src/modules/yookassa/yookassa-balance.service.ts
+++ src/modules/yookassa/yookassa-balance.service.ts
@@ -69,7 +69,7 @@ export class YookassaBalanceService {
           description: `Пополнение баланса user:${userId}`,
           confirmation: {
             type: 'redirect',
-            return_url: 'https://web.telegram.org/a/#7682387428',
+            return_url: 'tg://resolve?domain=passimx_vpn_bot',
           },
           metadata: {
             userId,
@@ -166,7 +166,7 @@ export class YookassaBalanceService {
     await this.em.update(
       TransactionEntity,
       { id: balancePayment.id },
-      { completed: true as const },
+      { completed: true },
     );
   }
 }
