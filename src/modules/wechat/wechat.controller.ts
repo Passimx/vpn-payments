856d36f813e94b8ddd61fad0760051ba1344010e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-04-10 17:45:55 +0800

    fix: correct remove user

--- src/modules/wechat/wechat.controller.ts
+++ src/modules/wechat/wechat.controller.ts
@@ -1,19 +1,16 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { WechatService } from './wechat.service';
-import type { InvoiceCallbackType } from './types/invoice-callback.type';
+import { Controller } from '@nestjs/common';
 
 @Controller('wechat')
 export class WechatController {
-  constructor(private wechatService: WechatService) {}
-
-  @Post('invoice/callback')
-  @HttpCode(200)
-  public async invoiceCallback(@Body() body: InvoiceCallbackType) {
-    await this.wechatService.invoiceCallback(body);
-
-    return {
-      code: 'SUCCESS',
-      message: 'OK',
-    };
-  }
+  // constructor(private wechatService: WechatService) {}
+  // @Post('invoice/callback')
+  // @HttpCode(200)
+  // public async invoiceCallback(@Body() body: InvoiceCallbackType) {
+  //   await this.wechatService.invoiceCallback(body);
+  //
+  //   return {
+  //     code: 'SUCCESS',
+  //     message: 'OK',
+  //   };
+  // }
 }
