d93a4d2cd51f14c0cf78f69f056507591353cc14
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-04-10 14:07:01 +0800

    fix: answer for wechat

--- src/modules/wechat/wechat.controller.ts
+++ src/modules/wechat/wechat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { WechatService } from './wechat.service';
 import type { InvoiceCallbackType } from './types/invoice-callback.type';
 
@@ -7,12 +7,13 @@ export class WechatController {
   constructor(private wechatService: WechatService) {}
 
   @Post('invoice/callback')
+  @HttpCode(200)
   public async invoiceCallback(@Body() body: InvoiceCallbackType) {
     await this.wechatService.invoiceCallback(body);
 
     return {
       code: 'SUCCESS',
-      message: 'SUCCESS',
+      message: 'OK',
     };
   }
 }
